031e69163118f4d83ab0cac9ecbe317f88f75d2b
Austin Belknap <dabelknap@wisc.edu>
2014-03-25 05:22:51 -0500

    Initial commit of library

---
+++ include/CaloLinks.h
@@ -0,0 +1,107 @@
+/**
+ * Filename: CaloLinks.h
+ *
+ * Description: Abstracts the JSC output for a single RCT crate into the
+ *              bit-fields used in the oRSC -> MP7/CTP7 optical links.
+ *
+ * Author: D. Ausitn Belknap, UW-Madison
+ */
+
+#ifndef _CALOLINKS_h
+#define _CALOLINKS_h
+
+#include <iostream>
+#include <stdexcept>
+#include <stdint.h>
+
+
+/**
+ * This class abstracts the JSC bits for a single RCT crate. It contains methods
+ * for defining the various RCT bits, and arranges them into the bit-fields used
+ * in the optical links.
+ */
+class CaloLinks {
+  private:
+    // RC Et, Index: RC number, region, bit number
+    uint8_t RCEt[7][2][10];
+
+    // RC Tau Veto, Index: RC number, region
+    uint8_t RCTau[7][2];
+
+    // RC Overflow, Index: RC number, region
+    uint8_t RCOf[7][2];
+
+    // RC HAD Veto (MIP), Index: RC number, region
+    uint8_t RCHad[7][2];
+
+    // HF Finegrain, Index: Region
+    uint8_t HFFg[8];
+
+    // HF Et, Index: Region, bit number
+    uint8_t HFEt[8][8];
+
+
+    // Non-Iso EG position (0 Region, 1-3 card)
+    // Index: candidate index, bit-number
+    uint8_t NEPos[4][4];
+
+    // Non-Iso EG Et, Index: cand. index, bit number
+    uint8_t NEEt[4][6];
+
+    // Iso EG position (0 Region, 1-3 card)
+    // Index: candidate index, bit-number
+    uint8_t IEPos[4][4];
+
+    // Iso EG Et, Index: cand. index, bit number
+    uint8_t IEEt[4][6];
+
+
+    uint8_t Link1 [24][8];
+    uint8_t Link2 [24][8];
+
+    bool table_filled;
+
+    static void check_RC_indicies(unsigned int card, unsigned int region);
+
+
+  public:
+    OrscLinks() { table_filled = false; };
+    ~OrscLinks() {};
+
+    void set_RCEt(unsigned int card, unsigned int region, unsigned int bit,
+        uint8_t value);
+    void set_RCTau(unsigned int card, unsigned int region, uint8_t value);
+    void set_RCOf(unsigned int card, unsigned int region, uint8_t value);
+    void set_RCHad(unsigned int card, unsigned int region, uint8_t value);
+
+    void set_HFFg(unsigned int region, uint8_t value);
+    void set_HFEt(unsigned int region, unsigned int bit, uint8_t value);
+
+    void set_NEPos(unsigned int cand, unsigned int bit, uint8_t value);
+    void set_NEEt(unsigned int cand, unsigned int bit, uint8_t value);
+
+    void set_IEPos(unsigned int cand, unsigned int bit, uint8_t value);
+    void set_IEEt(unsigned int cand, unsigned int bit, uint8_t value);
+
+
+    uint8_t get_RCEt(unsigned int card, unsigned int region, unsigned int bit);
+    uint8_t get_RCTau(unsigned int card, unsigned int region);
+    uint8_t get_RCOf(unsigned int card, unsigned int region);
+    uint8_t get_RCHad(unsigned int card, unsigned int region);
+
+    uint8_t get_HFFg(unsigned int region);
+    uint8_t get_HFEt(unsigned int region, unsigned int bit);
+
+    uint8_t get_NEPos(unsigned int cand, unsigned int bit);
+    uint8_t get_NEEt(unsigned int cand, unsigned int bit);
+
+    uint8_t get_IEPos(unsigned int cand, unsigned int bit);
+    uint8_t get_IEEt(unsigned int cand, unsigned int bit);
+
+
+    void populate_link_tables();
+
+    void print_table(int link);
+
+    std::vector<uint8_t> link_values(int link);
+};
---
+++ src/CaloLinks.cc
@@ -0,0 +1,348 @@
+/**
+ * Filename: OrscLinks.cc
+ *
+ * Description: Abstracts the output bits for a single RCT crate to be arranged
+ *              into bitfields used in the oRSC -> CTP7/MP7 optical links
+ *
+ * Author: D. Austin Belknap, UW-Madison
+ */
+
+#include "../include/CaloLinks.h"
+
+
+void
+CaloLinks::check_RC_indicies(unsigned int card, unsigned int region) {
+  if (card > 6) {
+    throw std::invalid_argument("Invalid RC card index");
+  }
+  if (region > 1) {
+    throw std::invalid_argument("Invalid RC region");
+  }
+}
+
+
+void
+CaloLinks::set_RCTEt(unsigned int card, unsigned int region, unsigned int bit,
+    uint8_t value) {
+  check_RC_indicies(card, region);
+  if (bit > 9) {
+    throw std::invalid_argument("Invalid RC Et bit index");
+  }
+  RCEt[card][region][bit] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_RCTEt(unsigned int card, unsigned int region, unsigned int bit) {
+  check_RC_indicies(card, region);
+  if (bit < 0 || bit > 9) {
+    throw std::invalid_argument("Invalid RC Et bit index");
+  }
+  return RCEt[card][region][bit];
+}
+
+
+void
+CaloLinks::set_RCTTau(unsigned int card, unsigned int region, uint8_t value) {
+  check_RC_indicies(card, region);
+  RCTau[card][region] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_RCTTau(unsigned int card, unsigned int region) {
+  check_RC_indicies(card, region);
+  return RCTau[card][region]
+}
+
+
+void
+CaloLinks::set_RCTOf(unsigned int card, unsigned int region, uint8_t value) {
+  check_RC_indicies(card, region);
+  RCOf[card][region] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_RCTOf(unsigned int card, unsigned int region) {
+  check_RC_indicies(card, region);
+  return RCOf[card][region]
+}
+
+
+void
+CaloLinks::set_RCTHad(unsigned int card, unsigned int region, uint8_t value) {
+  check_RC_indicies(card, region);
+  RCHad[card][region] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_RCTHad(unsigned int card, unsigned int region) {
+  check_RC_indicies(card, region);
+  return RCHad[card][region]
+}
+
+
+void
+CaloLinks::set_HFFg(unsigned int region, uint8_t value) {
+  if (region > 7) {
+    throw std::invalid_argument("Invalid HF region number");
+  }
+  HFFg[region] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_HFFg(unsigned int region) {
+  if (region > 7) {
+    throw std::invalid_argument("Invalid HF region number");
+  }
+  return HFFg[region];
+}
+
+
+void
+CaloLinks::set_HFEt(unsigned int region, unsigned int bit, uint8_t value) {
+  if (region > 7) {
+    throw std::invalid_argument("Invalid HF region number");
+  }
+  if (bit > 7) {
+    throw std::invalid_argument("Invalid HF Et bit number");
+  }
+  HFEt[region] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_HFEt(unsigned int region, unsigned int bit) {
+  if (region > 7) {
+    throw std::invalid_argument("Invalid HF region number");
+  }
+  if (bit > 7) {
+    throw std::invalid_argument("Invalid HF Et bit number");
+  }
+  return HFEt[region];
+}
+
+
+void
+CaloLinks::set_NEPos(unsigned int cand, unsigned int bit, uint8_t value) {
+  if (cand > 3) {
+    throw std::invalid_argument("Invalid NE candidate index");
+  }
+  if (bit > 3) {
+    throw std::invalid_argument("Invalid NE position bit index");
+  }
+  NEPos[cand][bit] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_NEPos(unsigned int cand, unsigned int bit) {
+  if (cand > 3) {
+    throw std::invalid_argument("Invalid NE candidate index");
+  }
+  if (bit > 3) {
+    throw std::invalid_argument("Invalid NE position bit index");
+  }
+  return NEPos[cand][bit];
+}
+
+
+void
+CaloLinks::set_NEEt(unsigned int cand, unsigned int bit, uint8_t value) {
+  if (cand > 3) {
+    throw std::invalid_argument("Invalid NE candidate index");
+  }
+  if (bit > 5) {
+    throw std::invalid_argument("Invalid NE Et bit index");
+  }
+  NEPos[cand][bit] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_NEEt(unsigned int cand, unsigned int bit) {
+  if (cand > 3) {
+    throw std::invalid_argument("Invalid NE candidate index");
+  }
+  if (bit > 5) {
+    throw std::invalid_argument("Invalid NE Et bit index");
+  }
+  return NEPos[cand][bit];
+}
+
+
+void
+CaloLinks::set_IEPos(unsigned int cand, unsigned int bit, uint8_t value) {
+  if (cand > 3) {
+    throw std::invalid_argument("Invalid IE candidate index");
+  }
+  if (bit > 3) {
+    throw std::invalid_argument("Invalid IE position bit index");
+  }
+  IEPos[cand][bit] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_IEPos(unsigned int cand, unsigned int bit) {
+  if (cand > 3) {
+    throw std::invalid_argument("Invalid IE candidate index");
+  }
+  if (bit > 3) {
+    throw std::invalid_argument("Invalid IE position bit index");
+  }
+  return IEPos[cand][bit];
+}
+
+
+void
+CaloLinks::set_IEEt(unsigned int cand, unsigned int bit, uint8_t value) {
+  if (cand > 3) {
+    throw std::invalid_argument("Invalid IE candidate index");
+  }
+  if (bit > 5) {
+    throw std::invalid_argument("Invalid IE Et bit index");
+  }
+  IEPos[cand][bit] = value & 0x1;
+}
+
+
+uint8_t
+CaloLinks::get_IEEt(unsigned int cand, unsigned int bit) {
+  if (cand > 3) {
+    throw std::invalid_argument("Invalid IE candidate index");
+  }
+  if (bit > 5) {
+    throw std::invalid_argument("Invalid IE Et bit index");
+  }
+  return IEPos[cand][bit];
+}
+
+
+/**
+ * Output the link values as 24 8-bit integers.
+ */
+std::vector<uint8_t>
+CaloLinks::link_values(int link_number) {
+  std::vector<uint8_t> link;
+
+  uint8_t val;
+
+  for (int i = 0; i < 24; i++) {
+    val = 0;
+
+    for (int j = 0; j < 8; j++) {
+      val <<= 1;
+
+      if (link_number == 1) {
+        val |= Link1[i][j] & 0x1;
+      }
+      else if (link_number == 2) {
+        val |= Link2[i][j] & 0x1;
+      }
+      else {
+          throw std::invalid_argument("Invalid link number given");
+      }
+    }
+    link.push_back(val);
+  }
+  return link;
+}
+
+
+void
+CaloLinks::print_table(int link) {
+  uint8_t (*Link)[24][8];
+
+  if (link == 1) {
+    Link = &Link1;
+  }
+  else if (link == 2) {
+    Link = &Link2;
+  }
+  else {
+    throw std::invalid_argument("Invalid link number given.");
+  }
+
+  for (int i = 0; i < 24; i++) {
+    for (int j = 0; j < 8; j++) {
+      std::cout << (int)(*Link)[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+
+/**
+ * Arrange the JSC output bits into the bitfields for the 2 oRSC optical links.
+ */
+void
+CaloLinks::populate_link_tables() {
+  uint8_t L1 [24][8] = {
+    {0,             0,              0,              0,              0,              0,              0,              0},
+    {0,             0,              0,              0,              0,              0,              0,              0},
+    {RCOf[6][1],    RCOf[6][0],     RCOf[5][1],     RCOf[5][0],     RCOf[4][1],     RCOf[4][0],     RCOf[3][1],     RCOf[3][0]},
+    {RCOf[2][1],    RCOf[2][0],     RCOf[1][1],     RCOf[1][0],     RCOf[0][1],     RCOf[0][0],     RCTau[6][1],    RCTau[6][0]},
+    {RCTau[5][1],   RCTau[5][0],    RCTau[4][1],    RCTau[4][0],    RCTau[3][1],    RCTau[3][0],    RCTau[2][1],    RCTau[2][0]},
+    {RCTau[1][1],   RCTau[1][0],    RCTau[0][1],    RCTau[0][0],    RCEt[6][1][9],  RCEt[6][1][8],  RCEt[6][1][7],  RCEt[6][1][6]},
+    {RCEt[6][1][5], RCEt[6][1][4],  RCEt[6][1][3],  RCEt[6][1][2],  RCEt[6][1][1],  RCEt[6][1][0],  RCEt[6][0][9],  RCEt[6][0][8]},
+    {RCEt[6][0][7], RCEt[6][0][6],  RCEt[6][0][5],  RCEt[6][0][4],  RCEt[6][0][3],  RCEt[6][0][2],  RCEt[6][0][1],  RCEt[6][0][0]},
+    {RCEt[5][1][9], RCEt[5][1][8],  RCEt[5][1][7],  RCEt[5][1][6],  RCEt[5][1][5],  RCEt[5][1][4],  RCEt[5][1][3],  RCEt[5][1][2]},
+    {RCEt[5][1][1], RCEt[5][1][0],  RCEt[5][0][9],  RCEt[5][0][8],  RCEt[5][0][7],  RCEt[5][0][6],  RCEt[5][0][5],  RCEt[5][0][4]},
+    {RCEt[5][0][3], RCEt[5][0][2],  RCEt[5][0][1],  RCEt[5][0][0],  RCEt[4][1][9],  RCEt[4][1][8],  RCEt[4][1][7],  RCEt[4][1][6]},
+    {RCEt[4][1][5], RCEt[4][1][4],  RCEt[4][1][3],  RCEt[4][1][2],  RCEt[4][1][1],  RCEt[4][1][0],  RCEt[4][0][9],  RCEt[4][0][8]},
+    {RCEt[4][0][7], RCEt[4][0][6],  RCEt[4][0][5],  RCEt[4][0][4],  RCEt[4][0][3],  RCEt[4][0][2],  RCEt[4][0][1],  RCEt[4][0][0]},
+    {RCEt[3][1][9], RCEt[3][1][8],  RCEt[3][1][7],  RCEt[3][1][6],  RCEt[3][1][5],  RCEt[3][1][4],  RCEt[3][1][3],  RCEt[3][1][2]},
+    {RCEt[3][1][1], RCEt[3][1][0],  RCEt[3][0][9],  RCEt[3][0][8],  RCEt[3][0][7],  RCEt[3][0][6],  RCEt[3][0][5],  RCEt[3][0][4]},
+    {RCEt[3][0][3], RCEt[3][0][2],  RCEt[3][0][1],  RCEt[3][0][0],  RCEt[2][1][9],  RCEt[2][1][8],  RCEt[2][1][7],  RCEt[2][1][6]},
+    {RCEt[2][1][5], RCEt[2][1][4],  RCEt[2][1][3],  RCEt[2][1][2],  RCEt[2][1][1],  RCEt[2][1][0],  RCEt[2][0][9],  RCEt[2][0][8]},
+    {RCEt[2][0][7], RCEt[2][0][6],  RCEt[2][0][5],  RCEt[2][0][4],  RCEt[2][0][3],  RCEt[2][0][2],  RCEt[2][0][1],  RCEt[2][0][0]},
+    {RCEt[1][1][9], RCEt[1][1][8],  RCEt[1][1][7],  RCEt[1][1][6],  RCEt[1][1][5],  RCEt[1][1][4],  RCEt[1][1][3],  RCEt[1][1][2]},
+    {RCEt[1][1][1], RCEt[1][1][0],  RCEt[1][0][9],  RCEt[1][0][8],  RCEt[1][0][7],  RCEt[1][0][6],  RCEt[1][0][5],  RCEt[1][0][4]},
+    {RCEt[1][0][3], RCEt[1][0][2],  RCEt[1][0][1],  RCEt[1][0][0],  RCEt[0][1][9],  RCEt[0][1][8],  RCEt[0][1][7],  RCEt[0][1][6]},
+    {RCEt[0][1][5], RCEt[0][1][4],  RCEt[0][1][3],  RCEt[0][1][2],  RCEt[0][1][1],  RCEt[0][1][0],  RCEt[0][0][9],  RCEt[0][0][8]},
+    {RCEt[0][0][7], RCEt[0][0][6],  RCEt[0][0][5],  RCEt[0][0][4],  RCEt[0][0][3],  RCEt[0][0][2],  RCEt[0][0][1],  RCEt[0][0][0]},
+    {0,             0,              0,              0,              0,              0,              0,              0}
+  };
+
+  uint8_t L2 [24][8] = {
+    {0,             0,              0,              0,              0,              0,              0,              0},
+    {0,             0,              0,              0,              0,              0,              0,              0},
+    {0,             0,              RCHad[6][1],    RCHad[6][0],    RCHad[5][1],    RCHad[5][0],    RCHad[4][1],    RCHad[4][0]},
+    {RCHad[3][1],   RCHad[3][0],    RCHad[2][1],    RCHad[2][0],    RCHad[1][1],    RCHad[1][0],    RCHad[0][1],    RCHad[0][0]},
+    {NEPos[3][3],   NEPos[3][2],    NEPos[3][1],    NEPos[3][0],    NEEt[3][5],     NEEt[3][4],     NEEt[3][3],     NEEt[3][2]},
+    {NEEt[3][1],    NEEt[3][0],     NEPos[2][3],    NEPos[2][2],    NEPos[2][1],    NEPos[2][0],    NEEt[2][5],     NEEt[2][4]},
+    {NEEt[2][3],    NEEt[2][2],     NEEt[2][1],     NEEt[2][0],     NEPos[1][3],    NEPos[1][2],    NEPos[1][1],    NEPos[1][0]},
+    {NEEt[1][5],    NEEt[1][4],     NEEt[1][3],     NEEt[1][2],     NEEt[1][1],     NEEt[1][0],     NEPos[0][3],    NEPos[0][2]},
+    {NEPos[0][1],   NEPos[0][0],    NEEt[0][5],     NEEt[0][4],     NEEt[0][3],     NEEt[0][2],     NEEt[0][1],     NEEt[0][0]},
+    {IEPos[3][3],   IEPos[3][2],    IEPos[3][1],    IEPos[3][0],    IEEt[3][5],     IEEt[3][4],     IEEt[3][3],     IEEt[3][2]},
+    {IEEt[3][1],    IEEt[3][0],     IEPos[2][3],    IEPos[2][2],    IEPos[2][1],    IEPos[2][0],    IEEt[2][5],     IEEt[2][4]},
+    {IEEt[2][3],    IEEt[2][2],     IEEt[2][1],     IEEt[2][0],     IEPos[1][3],    IEPos[1][2],    IEPos[1][1],    IEPos[1][0]},
+    {IEEt[1][5],    IEEt[1][4],     IEEt[1][3],     IEEt[1][2],     IEEt[1][1],     IEEt[1][0],     IEPos[0][3],    IEPos[0][2]},
+    {IEPos[0][1],   IEPos[0][0],    IEEt[0][5],     IEEt[0][4],     IEEt[0][3],     IEEt[0][2],     IEEt[0][1],     IEEt[0][0]},
+    {HFFg[7],       HFFg[6],        HFFg[5],        HFFg[4],        HFFg[3],        HFFg[2],        HFFg[1],        HFFg[0]},
+    {HFEt[7][7],    HFEt[7][6],     HFEt[7][5],     HFEt[7][4],     HFEt[7][3],     HFEt[7][2],     HFEt[7][1],     HFEt[7][0]},
+    {HFEt[6][7],    HFEt[6][6],     HFEt[6][5],     HFEt[6][4],     HFEt[6][3],     HFEt[6][2],     HFEt[6][1],     HFEt[6][0]},
+    {HFEt[5][7],    HFEt[5][6],     HFEt[5][5],     HFEt[5][4],     HFEt[5][3],     HFEt[5][2],     HFEt[5][1],     HFEt[5][0]},
+    {HFEt[4][7],    HFEt[4][6],     HFEt[4][5],     HFEt[4][4],     HFEt[4][3],     HFEt[4][2],     HFEt[4][1],     HFEt[4][0]},
+    {HFEt[3][7],    HFEt[3][6],     HFEt[3][5],     HFEt[3][4],     HFEt[3][3],     HFEt[3][2],     HFEt[3][1],     HFEt[3][0]},
+    {HFEt[2][7],    HFEt[2][6],     HFEt[2][5],     HFEt[2][4],     HFEt[2][3],     HFEt[2][2],     HFEt[2][1],     HFEt[2][0]},
+    {HFEt[1][7],    HFEt[1][6],     HFEt[1][5],     HFEt[1][4],     HFEt[1][3],     HFEt[1][2],     HFEt[1][1],     HFEt[1][0]},
+    {HFEt[0][7],    HFEt[0][6],     HFEt[0][5],     HFEt[0][4],     HFEt[0][3],     HFEt[0][2],     HFEt[0][1],     HFEt[0][0]},
+    {0,             0,              0,              0,              0,              0,              0,              0}
+  };
+
+
+  // copy values into the class member arrays
+  for (int i = 0; i < 24; i++) {
+    for (int j = 0; j < 8; j++) {
+      Link1[i][j] = L1[i][j];
+      Link2[i][j] = L2[i][j];
+    }
+  }
+
+  table_filled = true;
+}
