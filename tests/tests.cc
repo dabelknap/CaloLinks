de793f548ff03da6287a523f0aa7915222e85458
Austin Belknap <austin.belknap@gmail.com>
2014-04-07 10:24:00 +0200

    Use 2-event test file w/ new format

--- tests/tests.cc
+++ tests/tests.cc
@@ -4,14 +4,17 @@
 #include "CaloLinks.h"
 
 int main() {
-  std::ifstream infile("./data/example_output.txt");
+  std::ifstream infile("./data/two_events.txt");
   std::ofstream outfile("test.txt");
 
   CaloLinks links;
 
+  // read two events from the input file
   for (int i = 0; i < 2; ++i) {
+    // reads a single event from the file
     links.read_from_file(infile);
 
+    // write that event to an output file
     links.write_to_file(outfile);
   }
 
